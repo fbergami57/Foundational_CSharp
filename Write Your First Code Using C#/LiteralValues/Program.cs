df9a6a8c0f2c084fdf0d37abb82836fab23e9b44
fbergami57 <fabiobergami57@gmail.com>
2026-07-21 14:15:54 -0300

    restart

---
+++ Write Your First Code Using C#/LiteralValues/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("H,e,l,l,o");
+Console.WriteLine();
+
+Console.WriteLine('b');
+Console.WriteLine();
+
+Console.WriteLine("Hello World!"); // CS1012: Too many characters in character literal
+Console.WriteLine();
+
+//float
+Console.WriteLine(0.25F);
+Console.WriteLine();
+
+//double
+Console.WriteLine(2.625);
+Console.WriteLine();
+
+//decimal
+Console.WriteLine(12.3916m);
+Console.WriteLine(12.3916M);
+Console.WriteLine();
+
+//Boolean
+Console.WriteLine(true);
+Console.WriteLine(false);
+Console.WriteLine();
+
