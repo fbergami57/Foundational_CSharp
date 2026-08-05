993789f8692e398e3fb5ea420a49c7808abc1e4e
fbergami57 <fabiobergami57@gmail.com>
2026-08-04 22:49:10 -0300

    Interpolation recap

---
+++ Write Your First Code Using C#/Interpolation/Interpolation/Program.cs
@@ -0,0 +1,16 @@
+﻿string firstName = "Bergami";
+string greeting = "Hello";
+Console.WriteLine($"{greeting} {firstName}!");
+
+Console.WriteLine("\n");
+
+string firstName1 = "Bergami";
+string message = $"Hello {firstName1}";
+Console.WriteLine(message);
+
+Console.WriteLine("\n");
+
+int version = 11;
+string updateText = "Update to Windows";
+Console.WriteLine($"{updateText} {version}!");
+
