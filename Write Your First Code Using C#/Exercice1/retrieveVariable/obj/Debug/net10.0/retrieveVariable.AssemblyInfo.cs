2bf25e76241da5eed7583a7671127f4bfaf1e22e
Fabio Francisco Bergami <fabio.bergami@unimedfesp.coop.br>
2026-08-06 07:43:25 -0300

    challenge Unicode FespDell

--- Write Your First Code Using C#/Exercice1/retrieveVariable/obj/Debug/net10.0/retrieveVariable.AssemblyInfo.cs
+++ Write Your First Code Using C#/Exercice1/retrieveVariable/obj/Debug/net10.0/retrieveVariable.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("retrieveVariable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+993789f8692e398e3fb5ea420a49c7808abc1e4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e18ad8f5f145236a396da0dae965e0e88e9c6afe")]
 [assembly: System.Reflection.AssemblyProductAttribute("retrieveVariable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("retrieveVariable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Write Your First Code Using C#/Interpolation/Interpolation/obj/Debug/net10.0/Interpolation.AssemblyInfo.cs
+++ Write Your First Code Using C#/Interpolation/Interpolation/obj/Debug/net10.0/Interpolation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interpolation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+993789f8692e398e3fb5ea420a49c7808abc1e4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e18ad8f5f145236a396da0dae965e0e88e9c6afe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interpolation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interpolation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
