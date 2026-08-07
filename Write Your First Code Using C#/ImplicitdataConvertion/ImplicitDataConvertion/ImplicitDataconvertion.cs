8274c1018f02d65fea05d3c2e4bdd9f6d8720625
Fabio Francisco Bergami <fabio.bergami@unimedfesp.coop.br>
2026-08-07 15:57:46 -0300

    Math operations

---
+++ Write Your First Code Using C#/ImplicitdataConvertion/ImplicitDataConvertion/ImplicitDataconvertion.cs
@@ -0,0 +1,21 @@
+﻿int firstNumber = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNumber + secondNumber);
+
+Console.WriteLine();
+
+string firstName = "Bergami";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + widgetsSold + " widgets.");
+
+Console.WriteLine();
+
+string firstName2 = "Bergami";
+int widgetsSold2 = 7;
+Console.WriteLine(firstName2 + " sold " + widgetsSold2 + 7 + " widgets.");
+
+Console.WriteLine();
+
+string firstName3 = "Bergami";
+int widgetsSold3 = 7;
+Console.WriteLine(firstName3 + " sold " + (widgetsSold3 + 7) + " widgets. ");
--- Write Your First Code Using C#/LocalVariables/localVariables/obj/Debug/net10.0/localVariables.AssemblyInfo.cs
+++ Write Your First Code Using C#/LocalVariables/localVariables/obj/Debug/net10.0/localVariables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("localVariables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db2acabb8d81b6544b1df1d67d590232ee73727c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d91e995b97cc1cdce3ec7ab1253f9d5843ae36d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("localVariables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("localVariables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
