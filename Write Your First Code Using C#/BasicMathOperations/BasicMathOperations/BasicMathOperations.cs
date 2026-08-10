8b158a85959ba9134fe01426380c52abd0615d8b
Fabio Francisco Bergami <fabio.bergami@unimedfesp.coop.br>
2026-08-10 11:37:51 -0300

    update DellFesp

--- Write Your First Code Using C#/BasicMathOperations/BasicMathOperations/BasicMathOperations.cs
+++ Write Your First Code Using C#/BasicMathOperations/BasicMathOperations/BasicMathOperations.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quatient = 7 / 5;
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quatient);
