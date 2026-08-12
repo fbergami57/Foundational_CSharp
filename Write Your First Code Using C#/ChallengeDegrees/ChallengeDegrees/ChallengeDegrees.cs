45038599c871a74988595ccfa5102990fdbff8a6
Fabio Francisco Bergami <fabio.bergami@unimedfesp.coop.br>
2026-08-12 15:25:52 -0300

    Operations on numbers

---
+++ Write Your First Code Using C#/ChallengeDegrees/ChallengeDegrees/ChallengeDegrees.cs
@@ -0,0 +1,13 @@
+﻿int fahrenheit = 94;
+int celsius = fahrenheit - 32;
+
+Console.WriteLine((decimal)celsius * 5 / 9);
+
+//Other way to do it
+int fahrenheit2 = 94;
+decimal celsius2 = (fahrenheit2 - 32m) * 5m / 9m;
+
+Console.WriteLine("The temperature is " + celsius2 + "Celsius.");
+
+Console.WriteLine("Windows " + 1 + 1);
+Console.WriteLine(5 / 10);
